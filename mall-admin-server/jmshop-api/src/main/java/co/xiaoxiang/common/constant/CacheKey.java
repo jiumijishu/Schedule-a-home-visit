c8d5f735f84bb8611cf9bed5b10acc3ed693d9d3
王理想 <16705544+haungge@user.noreply.gitee.com>
2026-04-03 13:51:19 +0800

    同城上门

--- mall-admin-server/jmshop-api/src/main/java/co/xiaoxiang/common/constant/CacheKey.java
+++
@@ -1,7 +0,0 @@
-package co.xiaoxiang.common.constant;
-
-
-public interface CacheKey {
-    String ORDER_CANCEL_JOB = "order_cancel_job";
-
-}
--- mall-admin-server/jmshop-shop/src/main/java/co/xiaoxiang/modules/shop/service/dto/CountDto.java
+++
@@ -1,9 +0,0 @@
-package co.xiaoxiang.modules.shop.service.dto;
-
-import lombok.Data;
-
-//@Data
-public interface CountDto {
-
-     String getCatename();
-}
